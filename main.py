79d05d1d4b5e63d47c19af2a3a6ef2713c78f498
Carter Burn <899382+carterburn@users.noreply.github.com>
2024-12-28 23:15:41 -0500

    add player and player movement

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     _ = pygame.init()
@@ -10,13 +11,33 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     black = pygame.Color(0, 0, 0)
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
+    clock = pygame.time.Clock()
+    dt = 0
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+     
+        for sprite in updatable:
+            sprite.update(dt)
+
+        # render
         screen.fill(black)
 
+        for sprite in drawable:
+            sprite.draw(screen)
+
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
